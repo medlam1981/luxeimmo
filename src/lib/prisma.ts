594ce92c170b8e1febcda001ab6fde0e65477051
medlam1981 <medlam1981@users.noreply.github.com>
2026-07-18 20:23:33 +0000

    chore: intercept DB string to suppress pg sslmode=require warning

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,7 +3,12 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 
 const prismaClientSingleton = () => {
-  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  let connectionString = process.env.DATABASE_URL || '';
+  if (connectionString.includes('sslmode=require')) {
+    connectionString = connectionString.replace('sslmode=require', 'sslmode=verify-full');
+  }
+  
+  const pool = new Pool({ connectionString });
   const adapter = new PrismaPg(pool);
   return new PrismaClient({ adapter });
 };
