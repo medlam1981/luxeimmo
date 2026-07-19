4b340e5ba944c3238a9e25bf49ae16d29e07f34f
medlam1981 <medlam1981@users.noreply.github.com>
2026-07-19 05:34:09 +0000

    chore: remove temporary test scripts

--- test_translate.mjs
+++
@@ -1,7 +0,0 @@
-import translate from 'translate';
-translate.engine = 'google';
-try {
-  console.log(await translate('The legal guide to buying property in Morocco', { to: 'ar' }));
-} catch (e) {
-  console.error(e);
-}
